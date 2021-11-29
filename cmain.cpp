fedba364e06e3cec384a6138bb59a02e5aba5381
amradel <amroadel@aucegypt.edu>
2021-11-29 06:39:09 +0200

    Ready to submit

--- cmain.cpp
+++ cmain.cpp
@@ -1,7 +1,12 @@
 /*
+Work by: 
+Ismail Shaheen 900171417 i.shaheen@aucegypt.edu
+Amr Abdalla 900163149   amroadel@aucegpyt.edu
+
+The Reliability Experiment:
 To test the reliability of datagrams, we tried sending a message with legnth longer than the buffer size. 
 The message recived by the server was croped to match the buffer size. For example: 
-We sett BUFSIZE to 4 and sent a message of 0123456789
+We sett BUFSIZE to 4 and sent a message of 0123456789.
 The server received and echoed only 012 to the client because marshalling increases the size of the message, and 
 every 3 bytes in the original message are represented using 4 bytes. In unmarshalling, every 4 bytes are represented by 3 bytes. 
 This experiment shows that UDP has issues with reliability backet loss.  
